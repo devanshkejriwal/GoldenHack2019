049ae1db69fb4b1c5ee7f3ee8df02fbb728e7866
unknown <devanshkejriwal@gmail.com>
2019-10-05 21:17:53 -0400

    Pushed basic code

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const bodyParser = require("body-parser");
 
 // Database setup
-mongoose.connect('mongodb://localhost:27017/goldenhacks', {useNewUrlParser: true});
+mongoose.connect('mongodb://localhost:27017/goldenhack', {useNewUrlParser: true});
 const db = mongoose.connection
 db.on('error', console.error.bind(console, 'connection error:'))
 db.once('open', function() {
@@ -13,6 +14,7 @@ db.once('open', function() {
 const categorySchema = mongoose.Schema({
   id: Number,
   title: String,
+  subcategory: [String]
 }, { collection: 'categories'})
 
 // Application setup
@@ -22,17 +24,43 @@ app.listen(port, () => {
   console.log(`App is listening on port ${port}`)
 })
 
+//Set view engine to ejs
+app.set("view engine", "ejs");
+
+//Tell Express where we keep our index.ejs
+app.set("views", __dirname + "/views");
+
+//Use body-parser
+app.use(bodyParser.urlencoded({ extended: false }));
+
 
 // API's
 app.get('/', (req,res) => {
   res.send("Hello World");
 })
 
-app.get('/category', (req,res) => {
-  // Getting name from url. Use format /search?name=tobi+ferret to get "tobi ferret"
-  var companyName = req.query.name;
-
+app.get('/createcategory', (req,res) => {
   //Sample query
-  var Category = mongoose.model('Category',companySchema)
-  Category.create({title:'Sports', id: 1})
+  var Category = mongoose.model('Category',categorySchema)
+  var sports = ['Squash', 'Badminton', 'Pool', 'Swimming', 'Basketball', 'MMA']
+  var movies = ['In Cinemas']
+  var adventure = ['Rock Climbing', 'Axe Throwing', 'Ice Skating', 'Trampoline Park']
+  var shopping = ['Shopping']
+  var events = ['Concerts', 'Theatre', 'Parties']
+
+  var insertCategories = [
+    {title:'Sports', id: 1, subcategory: sports},
+    {title:'Movies', id: 2, subcategory: movies},
+    {title:'Adventure', id: 3, subcategory: adventure},
+    {title:'Shopping', id: 4, subcategory: shopping},
+    {title:'Events', id: 5, subcategory: events}
+
+  ]
+
+  insertCategories.forEach(function(category) {
+    Category.findOneAndUpdate(category,category, {upsert: true}, function(err, doc) {
+      console.log(doc)
+    })
+  })
+  res.render("index", {sports_ejs: sports})
 })
